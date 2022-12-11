05c5621fd4ea00470640465613816333e2dd2112
rfire <rfire01@gmail.com>
2022-12-11 08:22:03 +0200

    hide without user + added pie chart

--- frontend.py
+++ frontend.py
@@ -1,5 +1,9 @@
+import re
+
 from flask import Flask, render_template, request, url_for, flash, redirect
 import dictdatabase as DDB
+
+from config import REQUIRED_POINTS
 
 app = Flask(__name__)
 
@@ -7,11 +11,25 @@ app = Flask(__name__)
 @app.route("/", methods=["GET", "POST"])
 def home():
     score = 0
+    email = ''
+    show_output = False
+    valid_user = False
     if request.method == "POST":
-        if DDB.at('scores', key=request.form["email"]).exists():
-            score = DDB.at('scores', key=request.form["email"]).read()
+        email = re.sub('[^A-Za-z0-9@.]+', '', request.form["email"])
+        # print(email, DDB.at('scores', key=email).exists())
+        if DDB.at('scores', key=email).exists():
+            score = DDB.at('scores', key=email).read()
+            valid_user = True
 
-    return render_template("home.html", score=score, requirement=200)
+        show_output = True
+
+    data = {'Task' : 'Score required', 'Accumulated Score' : score,
+            'Required Score' : max(0, REQUIRED_POINTS - score)}
+
+    params = {'score':score, 'requirement':REQUIRED_POINTS, 'email':email,
+              'show_output': show_output, 'valid_user': valid_user,
+              'data': data}
+    return render_template("home.html", **params)
 
 
 if __name__ == "__main__":
